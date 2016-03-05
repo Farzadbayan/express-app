e8682ed380d43abe80ff961ad6efcbc3f6ebae36
Farzad Bayan <farzadbayan@gmail.com>
2016-03-05 18:45:39 +0330

    Created main.handlebars

--- app.js
+++ app.js
@@ -2,25 +2,27 @@ var express = require('express');
 
 var app = express();
 
+var handlebars = require('express3-handlebars').create({ defaultLayout: 'main' });
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', function(req, res){
-  res.type('text/plain');
-  res.send('Welcome to Home Page');
+  res.render('home');
 });
 
-app.get('about', function(req, res){
-  res.type('text/plain');
-  res.send('About Page');
-})
+app.get('/about', function(req, res){
+  res.render('about');
+});
 
 // custom 404 page
 app.use(function(req, res, next){
   res.status(404);
-  res.type('text/plain');
-  res.send('Page Not Found.');
+  res.render('404');
 });
 
 app.listen(app.get('port'), function(){
   console.log('now listening to port 3000')
-})
+});
