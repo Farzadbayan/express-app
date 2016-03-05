276617f12647375bb014a66329b5cd1e1de9b4ec
Farzad <Farzad@Apples-MacBook-Pro.local>
2016-03-05 17:36:06 +0330

    Created app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.get('/', function(req, res){
+  res.type('text/plain');
+  res.send('Home Page');
+});
+
+app.get('about', function(req, res){
+  res.type('text/plain');
+  res.send('About Page');
+})
+
+// custom 404 page
+app.use(function(req, res, next){
+  res.status(404);
+  res.type('text/plain');
+  res.send('Page Not Found.');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('now listening to port 3000')
+})
