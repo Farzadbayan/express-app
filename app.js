245cbb0bdfc13f365b565b4aca89b090afb9a512
Farzad Bayan <farzadbayan@gmail.com>
2016-03-06 10:29:23 +0330

    Getting started with QA

--- app.js
+++ app.js
@@ -12,6 +12,12 @@ app.use(express.static(__dirname + '/public'));
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(function(req, res, next){
+  res.locals.showTests = app.get('env') !== 'production' &&
+    req.query.test == '1';
+    next();
+});
+
 app.get('/', function(req, res){
   res.render('home', {quote: quotes.getQuote()});
 });
